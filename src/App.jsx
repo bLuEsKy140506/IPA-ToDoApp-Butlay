7a0cc0217c0c6da39912ad739b611feaba974206
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2023-11-01 16:36:30 +0800

    comments for future references

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,21 @@ function App() {
           item features
         </h3>
         <br /> */}
-        <h2>SOLVING IPA-5</h2>
+        <h2>SOLVING Individual Project Assignment 5</h2>
         <NewTodoForm className="plus" />
       </div>
       <hr style={{ marginBottom: "5px" }} />
       <div className="container">
+        <h5
+          style={{
+            textAlign: "center",
+            marginBottom: "20px",
+          }}
+        >
+          THIS IS FOR DEMO PURPOSES -- DRAGGING WILL NOT WORK ON MOBILE PHONE
+          AND TASK RECENTLY ADDED WILL NOT BE DRAG BECAUSE THIS DEPLOYMENT HAS
+          NOT LINKED TO ANY DATABASE SERVER
+        </h5>
         <h4>
           To replace the FOCUS TASK just drag the card to the FOCUS task
           top-border until the green color appears
